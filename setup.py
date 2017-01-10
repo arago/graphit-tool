c0300a959676f3cbe3d6fac789f63d8191c34a06
stormking <stormking@web.de>
2017-01-10 18:00:42 +0100

    Add setup.py, MANIFEST and default config

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2
+
+import distutils.core
+
+name = 'graphit-tool'
+
+distutils.core.setup(name=name,
+    version='0.1',
+    author="Marcus Klemm",
+    author_email="mklemm@arago.de",
+    url="https://arago.co",
+    description="Tool to manipulate GraphIT",
+    long_description="Tool to manipulate GraphIT",
+
+    scripts=['graphit-tool.py'],
+    py_modules=['graphit'],
+    data_files=[('/etc', ['conf/graphit-tool.conf'])]
+)
